2cd388918f37055a105d809ecd87483085bf61c9
“Erikas <erikas.pasiklydes@gmail.com>
2023-01-22 14:36:21 +0200

    Kliento(slaptazodzio keitimas)

--- ec/app/urls.py
+++ ec/app/urls.py
@@ -3,7 +3,7 @@ from . import views
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib.auth import views as auth_view
-from .forms import LoginForm, MyPasswordResetForm
+from .forms import LoginForm, MyPasswordResetForm, MyPasswordChangeForm
 
 
 urlpatterns = [
@@ -22,6 +22,10 @@ urlpatterns = [
     path('accounts/login/', auth_view.LoginView.as_view(template_name='app/login.html', authentication_form=LoginForm),
          name='login'),
     path('password-reset/', auth_view.PasswordResetView.as_view(template_name='app/password_reset.html',
-    form_class=MyPasswordResetForm), name='password_reset'),
+        form_class=MyPasswordResetForm), name='password_reset'),
+    path('password_change/', auth_view.PasswordChangeView.as_view(template_name='app/password_change.html',
+        form_class=MyPasswordChangeForm, success_url='/password_change_done'), name='password_change'),
+    path('password_change_done/', auth_view.PasswordChangeDoneView.as_view(
+        template_name='app/password_change_done.html'), name='password_change_done'),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
