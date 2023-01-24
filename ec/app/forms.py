9317c69984fa060256ae4c50c6d186f2f957f7f7
“Erikas <erikas.pasiklydes@gmail.com>
2023-01-25 00:30:41 +0200

    Pass resset

--- ec/app/forms.py
+++ ec/app/forms.py
@@ -1,5 +1,6 @@
 from django import forms
-from django.contrib.auth.forms import UserCreationForm, AuthenticationForm, UsernameField, PasswordChangeForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm, UsernameField, PasswordChangeForm,\
+    SetPasswordForm
 from django.contrib.auth.models import User
 from .models import Customer
 
@@ -30,7 +31,15 @@ class MyPasswordChangeForm(PasswordChangeForm):
 
 
 class MyPasswordResetForm(PasswordChangeForm):
-    pass
+    email = forms.EmailField(widget=forms.EmailInput(attrs={'class': 'form-control'}))
+
+
+
+class MySetPasswordForm(SetPasswordForm):
+    new_password1 = forms.CharField(label='New Password', widget=forms.PasswordInput(attrs=
+        {'autocomplete': 'current-password', 'class': 'form-control'}))
+    new_password2 = forms.CharField(label='Confirm New Password', widget=forms.PasswordInput(attrs=
+        {'autocomplete': 'current-password', 'class': 'form-control'}))
 
 
 class CustomerProfileForm(forms.ModelForm):
