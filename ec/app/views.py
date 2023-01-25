7cc5e950ec457e9dd8ee8131b5bdd3498ee0422e
“Erikas <erikas.pasiklydes@gmail.com>
2023-01-26 00:39:02 +0200

    Prekes dedamos i krepseli

--- ec/app/views.py
+++ ec/app/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render, redirect
 from django.views import View
-from .models import Product, Customer
+from .models import Product, Customer, Cart
 from .forms import CustomerRegistrationForm, CustomerProfileForm
 from django.contrib import messages
+from django.http import JsonResponse
+from django.db.models import Q
 
 
 # Create your views here.
@@ -100,3 +102,122 @@ class updateAddress(View):
         else:
             messages.warning(request, "Invalid Input Data")
         return redirect("address")
+
+#### Jeigu noreciau , kad ta pati preke tik kitos spalvos ideti i krepseli
+# def add_to_cart(request):
+#     user = request.user
+#     product_id = request.GET.get('prod_id')
+#     if Cart.objects.filter(product=product_id).exists():
+#         prod_id = request.GET['prod_id']
+#         c = Cart.objects.get(Q(product=prod_id) & Q(user=request.user))
+#         c.quantity += 1
+#         c.save()
+#         user = request.user
+#         cart = Cart.objects.filter(user=user)
+#         amount = 0
+#         for p in cart:
+#             value = p.quantity * p.product.discounted_price
+#             amount = amount + value
+#         totalamount = amount + 3.50
+#         print(prod_id)
+#         data = {
+#             'quantity': c.quantity,
+#             'amount': amount,
+#             'totalamount': totalamount
+#         }
+#         return redirect('/cart')
+#     else:
+#         pass
+#         product = Product.objects.get(id=product_id)
+#         Cart(user=user, product=product).save()
+#         return redirect('/cart')
+
+# arba
+
+def add_to_cart(request):
+    user = request.user
+    product_id = request.GET.get('prod_id')
+    product = Product.objects.get(id=product_id)
+    Cart(user=user, product=product).save()
+    return redirect('/cart')
+
+
+def show_cart(request):
+    user = request.user
+    cart = Cart.objects.filter(user=user)
+    amount = 0
+    for p in cart:
+        value = p.quantity * p.product.discounted_price
+        amount = amount + value
+    totalamount = amount + 3.50
+    return render(request, 'app/addtocart.html', locals())
+
+
+
+class checkout(View):
+    def get(self, request):
+        return render(request, 'app/checkout.html', locals())
+
+
+def plus_cart(request):
+    if request.method == 'GET':
+        prod_id = request.GET['prod_id']
+        c = Cart.objects.get(Q(product=prod_id) & Q(user=request.user))
+        c.quantity += 1
+        c.save()
+        user = request.user
+        cart = Cart.objects.filter(user=user)
+        amount = 0
+        for p in cart:
+            value = p.quantity * p.product.discounted_price
+            amount = amount + value
+        totalamount = amount + 3.50
+        data = {
+            'quantity': c.quantity,
+            'amount': amount,
+            'totalamount': totalamount
+        }
+        return JsonResponse(data)
+
+
+
+def minus_cart(request):
+    if request.method == 'GET':
+        prod_id = request.GET['prod_id']
+        c = Cart.objects.get(Q(product=prod_id) & Q(user=request.user))
+        c.quantity -= 1
+        c.save()
+        user = request.user
+        cart = Cart.objects.filter(user=user)
+        amount = 0
+        for p in cart:
+            value = p.quantity * p.product.discounted_price
+            amount = amount + value
+        totalamount = amount + 3.50
+        data = {
+            'quantity': c.quantity,
+            'amount': amount,
+            'totalamount': totalamount
+        }
+        return JsonResponse(data)
+
+
+
+def remove_cart(request):
+    if request.method == 'GET':
+        prod_id = request.GET['prod_id']
+        c = Cart.objects.get(Q(product=prod_id) & Q(user=request.user))
+        c.delete()
+        user = request.user
+        cart = Cart.objects.filter(user=user)
+        amount = 0
+        for p in cart:
+            value = p.quantity * p.product.discounted_price
+            amount = amount + value
+        totalamount = amount + 3.50
+        data = {
+            'quantity': c.quantity,
+            'amount': amount,
+            'totalamount': totalamount
+        }
+        return JsonResponse(data)
